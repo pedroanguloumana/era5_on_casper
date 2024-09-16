d07dfc2b1dc7c1c59b7d8536468df9f4229b26f8
Pedro Angulo-Umana <pangulo@casper-login2.hpc.ucar.edu>
2024-09-15 23:35:13 -0600

    Regular

--- daily_mean_3d.sh
+++ daily_mean_3d.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export TMPDIR=${SCRATCH}/temp
+mkdir -p ${TMPDIR}
+
 module load cdo
 
 # Load configs
@@ -7,9 +10,12 @@ source $(dirname "$0")/configs.sh
 
 # Set years and months
 YEARS_TO_PROCESS=$(seq 2000 2000)
-MONTHS_TO_PROCESS=$(seq 1 12)
+MONTHS_TO_PROCESS=$(seq 1 1)
 VARIABLE_NAME_SHORT="q"
 VARIABLE_NAME_LONG="specific_humidity"
+
+OUT_PATH="${OUTPUT_DIR}/daily_means/${VARIABLE_NAME_LONG}"
+mkdir -p ${OUT_PATH}
 
 for year in $YEARS_TO_PROCESS; do
   for month in $MONTHS_TO_PROCESS; do
@@ -17,24 +23,20 @@ for year in $YEARS_TO_PROCESS; do
     echo "Processing Year: $year :: Month: $month"
     year_month_dir=${DATA_DIR_3D}/${year}${month}
 
-    # get files corresponding to variable
+    # Get files corresponding to variable
     files=$(find "$year_month_dir" -type f -name "e5.oper.an.pl.1*_${VARIABLE_NAME_SHORT}.*.nc" | sort)
+
     # Check if the 'files' variable is empty
     if [ -z "$files" ]; then
         echo "No files found in $year_month_dir for pattern e5.oper.an.pl.1*_${VARIABLE_NAME_SHORT}.*.nc"
         exit 1  # Exit with status 1 if no files are found
     else
-        echo "Found files: $files"
-        for file in $files; do
-        echo ${file}
-        # output_file="${file%.nc}_dailymean.nc"
-
-
-        # # Use cdo to compute daily means
-        # cdo daymean "$file" "$output_file"
-        # echo "Created daily mean file: $output_file"
-        done 
+        for file in ${files}; do
+            file_base=$(basename "$file")
+            cdo remapcon,r360x180 ${file} ${TMPDIR}/remap.${file_base}
+            cdo daymean ${TMPDIR}/remap.${file_base} ${TMPDIR}/daymean.${file_base}
+        done
+        cdo mergetime daymean* "${OUT_PATH}/daily.${year}.${month}.nc"
     fi
   done
 done
-
